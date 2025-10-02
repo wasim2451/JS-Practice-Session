b456984471e7f0ce1e27be11e1c80e6392704622
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-10-03 02:00:48 +0530

    Rev a String

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("hello")
+// Palindrom String
+function checkPalindrome(str){
+    const arr=str.split('');
+    console.log(arr);
+    
+    arr.reverse();
+    console.log(arr);
+    
+    const revstr=arr.join('');
+    return str==revstr;
+}
+console.log(checkPalindrome("madam"));
