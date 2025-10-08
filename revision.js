179870dc39d86b8a3815db146a7a8d6dae0648a7
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-10-08 22:02:27 +0530

    Page 1 Revised !

---
+++ revision.js
@@ -0,0 +1,71 @@
+// Modules : module.exports ={...something}
+// export const a ; export default b ; // import xyz from b // import {a} from b ;
+//Dynamic import means : import('./yz) 
+
+//Sorting Methods
+// let a=[3,2,10,5,12];
+// a.sort((a,b)=>b-a);
+// a.sort((a,b)=>a-b);
+// console.log(a);
+// let arr=[
+//     {name:"wasim",age:22},
+//     {name:"sritama",age:20}
+// ]
+// arr.sort((a,b)=>b.age-a.age);
+// console.log(arr);
+
+
+// Split
+// let str="hello";
+// let arr=str.split('');
+// //[h,e,l,l,o]
+// console.log(arr);
+
+//Join
+// let arr=[1,2,4,5,6,7,8,9];
+// let str=arr.join("|");
+// console.log(str);
+
+// Rev a string
+// let str="hello world";
+// let arr=str.split('');
+// arr.reverse();
+// str=arr.join('');
+// console.log(str);
+
+//Spread Operator
+// let arr=[2,4];
+// let brr=[...arr,9,0,8];
+// console.log(brr);
+// function abc(x,y,z){
+//     console.log(arguments);
+    
+//     return x+y+z;
+// }
+// let x=[1,10,5,20];
+// console.log(abc(...x));
+
+// let s="Sritama Kumar"
+// let sr=[...s];
+// console.log(sr);
+
+// // Spread in Math Operator 
+// let x=[1,2,3,5];
+// console.log(Math.min(x)); //NaN
+// console.log(Math.min(...x)); //NaN
+
+// Destructuring 
+// let [a,b]=[20,30];
+// console.log({a});
+// console.log({b});
+
+// //swapping
+// [a,b]=[b,a]
+// console.log({a});
+// console.log({b});
+
+
+
+
+
+
